6c40b9dfed043c13a2c9d83838c7a522abed6e16
KittenAmogus <kittenamogus3@gmail.com>
2026-06-04 11:47:07 +0300

    Clear all, start again

--- flush.sh
+++ flush.sh
@@ -1,6 +1,32 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
-# Цикл работает, ПОКА файл /dev/sda НЕ существует
-while [ ! -e /dev/sda ]; do
+export INKPAD_DISK=/dev/sda
+
+# Wait for InkPad
+echo "-- Waiting for '$INKPAD_DISK'..."
+while [ ! -e "$INKPAD_DISK" ]; do
   sleep 0.5
 done
+echo "-- Found InkPad"
+
+if [ -e "${INKPAD_DISK}1" ]; then
+  echo "Disk '$INKPAD_DISK' is not InkPad!"
+  echo "Change flush.sh"
+else
+  # Mount InkPad
+  sudo mount "$INKPAD_DISK" /mnt
+  echo "-- Mounted"
+
+  # Copy build to InkPad
+  sudo cp ./app.app /mnt/applications/sweeper.app
+  echo "-- Copied app"
+
+  # Copy icons
+  sudo cp ./icon.bmp /mnt/applications/icons/sweeper.bmp
+  sudo cp ./icon_f.bmp /mnt/applications/icons/sweeper_f.bmp
+  echo "-- Copied icons"
+
+  # Unmount InkPad
+  sudo umount /mnt
+  echo "-- Unmounted"
+fi
--- src/main.c
+++ src/main.c
@@ -1,191 +1,48 @@
-// Standart libs
-#include <stdint.h>
-#include <stdlib.h>
-
-// InkPad lib
+// === Libraries ===
 #include "inkview.h"
 
-// Sweeper libs
-#include "cell.h"
-#include "game.h"
-#include "config.h"
-#include "images.h"
+// === App event handlers ===
+// Allocate memory, create timers
+void onInit(void) {}
 
-// Colors
-#define COL_LIGHTGREY 0xCCCCCC
-#define COL_DARKGREY  0x333333
-#define COL_BLACK     0x000000
-#define COL_WHITE     0xFFFFFF
+// Free memory, timers
+void onExit(void) {}
 
-uint32_t getRandom(void) {
-  static uint32_t seed = 0x19236AF;
+// === Event handlers ===
+// Screen events
+void onShow(void) {}
+void onHide(void) {}
 
-  seed ^= (seed << 13);
-  seed ^= (seed >> 17);
-  seed ^= (seed << 5);
+// Key events
+void onKeyPress(int key) {}
+void onKeyDown(int key) {}
+void onKeyUp(int key) {}
 
-  return seed;
-}
+// Pointer events
+void onPtrMove(int x, int y) {}
+void onPtrDown(int x, int y) {}
+void onPtrUp(int x, int y) {}
 
-
-// Macros
-#define GET_CELL(x, y) (&(cells[x + (y * CellsX)]))
-
-#define MIN(a, b) (a > b ? b : a)
-#define MAX(a, b) (a < b ? b : a)
-#define CLAMP(amin, amax, a) (MIN(MAX(a, amin), amax))
-
-// Variables
-Cell *cells;
-Game game;
-
-uint8_t CellsX;
-uint8_t CellsY;
-
-int8_t cellSizeX;
-int8_t cellSizeY;
-
-
-// Game functions
-void genMines(void) {
-  game.allMines = 10;
-
-  uint8_t tries = 0;
-
-  while (game.remainingMines < game.allMines) {
-    ++tries;
-    if (tries == 0) break;
-
-    uint8_t posX = getRandom() % CellsX;
-    uint8_t posY = getRandom() % CellsY;
-
-    Cell *cell = GET_CELL(posX, posY);
- 
-    if (cell->isMine) continue;
-
-    cell->isMine = 1;
-    game.remainingMines++;
-
-    for (int8_t ay=posY-1; ay<posY+2; ++ay) {
-      if (ay < 0 || ay >= CellsY) continue;
-      for (int8_t ax=posX-1; ax<posX+2; ++ax) {
-        if (ax < 0 || ax >= CellsX) continue;
-        if (ax == posX && ay == posY) continue;
-        cell = GET_CELL(ax, ay);
-        cell->nearMines++;
-        cell->isOpen = 1;
-      }
-    }
-  }
-}
-
-
-void redraw(void) {
-}
-
-#define DRAW_AREA(x, y, col) FillArea(\
-  x * cellSizeX,\
-  y * cellSizeY,\
-cellSizeX, cellSizeY, col)
-
-void redrawFull(void) {
-  for (uint8_t y=0; y<CellsY; y++) {
-    for (uint8_t x=0; x<CellsX; x++) {
-      Cell *cell = GET_CELL(x, y);
-
-      if (!cell->isOpen) {
-        if (cell->isFlag) {
-          DRAW_AREA(x, y, COL_WHITE);
-          DrawRect(x * cellSizeX, y * cellSizeY, cellSizeX, cellSizeY, COL_BLACK);
-        } else {
-          DRAW_AREA(x, y, COL_DARKGREY);
-        }
-      } else {
-        if (cell->isMine) {
-          DRAW_AREA(x, y, COL_BLACK);
-        } else {
-          DRAW_AREA(x, y, COL_LIGHTGREY);
-        }
-      }
-
-    }
-  }
-
-  PartialUpdate(0, 0,
-                CellsX * cellSizeX,
-                CellsY * cellSizeY);
-}
-
-
-// Functions
-uint8_t start(void) {
-  CellsX = 9;
-  CellsY = 9;
-
-  // Create cells array
-  if (cells == NULL)
-    cells = malloc(sizeof(Cell) * (CellsY * CellsX));
-  if (cells == NULL) return 0;
-
-  // Reset everything
-  memset(&game, 0, sizeof(Game));
-  memset(cells, 0, sizeof(Cell) * (CellsY * CellsX));
-
-  // Create mines and draw everything
-  genMines();
-  redrawFull();
-
-  return 1;
-}
-
-
-uint8_t onInit(void) {
-  /*cellSizeX = ScreenWidth() / CellsX;
-  cellSizeY = ScreenHeight() / CellsY;
-  cellSizeX = MIN(cellSizeX, cellSizeY) - 4;
-  cellSizeY = cellSizeX;*/
-  cellSizeX = 64;
-  cellSizeY = 64;
-  return start();
-}
-
-void onExit(void) {
-  if (cells != NULL) free(cells);
-  cells = NULL;
-
-  CloseApp();
-}
-
+// === Entries ===
 // InkPad entry
 int main_handler(int event, int p1, int p2) {
   // App start and exit
-  if (event == EVT_INIT) {
-    if (!onInit()) onExit();
-  }
+  if (event == EVT_INIT)      onInit();
   else if (event == EVT_EXIT) onExit();
 
-  else if (event == EVT_KEYPRESS) start();
+  // Screen handlers
+  else if (event == EVT_SHOW) onShow();
+  else if (event == EVT_HIDE) onHide();
 
-  else if (event == EVT_POINTERDOWN) {
-    // X=p1, Y=p2
-    uint8_t intX, intY;
-    intX = (p1 / cellSizeX);
-    intY = (p2 / cellSizeY);
-    if ((
-        intX >= 0 && intX < (CellsX * cellSizeX)) &&
-        (intY >= 0 && intY < (CellsY * cellSizeY))) {
+  // Key handlers
+  else if (event == EVT_KEYPRESS) onKeyPress(p1);
+  else if (event == EVT_KEYDOWN)  onKeyDown(p1);
+  else if (event == EVT_KEYUP)    onKeyUp(p1);
 
-      Cell *cell = GET_CELL(intX, intY);
-
-      if (!cell) return 1;
-
-      if (!cell->isFlag)
-        cell->isOpen = 1;
-      redrawFull();
-    } else {
-      Message(ICON_INFORMATION, "Help", "ok", 100);
-    }
-  }
+  // Pointer(touch) handlers
+  else if (event == EVT_POINTERMOVE)  onPtrMove(p1, p2);
+  else if (event == EVT_POINTERDOWN)  onPtrDown(p1, p2);
+  else if (event == EVT_POINTERUP)    onPtrUp(p1, p2);
 
   return 0;
 }
